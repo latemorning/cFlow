5ae78a8da7c165832d24a711ae36d656e21cf758
kim hakjin <latemorning@gmail.com>
2024-07-18 10:38:55 +0900

    PaginationInfo 객체 추가

--- src/main/java/egovframework/com/cmm/util/ApiResponse.java
+++ src/main/java/egovframework/com/cmm/util/ApiResponse.java
@@ -2,6 +2,7 @@ package egovframework.com.cmm.util;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
 
 @Getter
 @RequiredArgsConstructor
@@ -9,22 +10,24 @@ public class ApiResponse<E> {
 
   private ApiHeader header;
   private E[] datas;
+  private PaginationInfo paginationInfo;
   private String msg;
 
   private static final int SUCCESS = 200;
 
-  private ApiResponse(ApiHeader header, E[] datas, String msg) {
+  private ApiResponse(ApiHeader header, E[] datas, PaginationInfo paginationInfo, String msg) {
     this.header = header;
     this.datas = datas;
+    this.paginationInfo = paginationInfo;
     this.msg = msg;
   }
 
-  public static <E> ApiResponse<E> success(E[] datas, String message) {
-    return new ApiResponse<>(new ApiHeader(SUCCESS, "SUCCESS"), datas, message);
+  public static <E> ApiResponse<E> success(E[] datas, PaginationInfo paginationInfo, String message) {
+    return new ApiResponse<>(new ApiHeader(SUCCESS, "SUCCESS"), datas, paginationInfo, message);
   }
 
   public static <E> ApiResponse<E> fail(ApiResponseCode responseCode, E[] datas) {
-    return new ApiResponse<>(new ApiHeader(responseCode.getHttpStatusCode(), responseCode.getMessage()), datas,
+    return new ApiResponse<>(new ApiHeader(responseCode.getHttpStatusCode(), responseCode.getMessage()), datas, null,
         responseCode.getMessage());
   }
 }
