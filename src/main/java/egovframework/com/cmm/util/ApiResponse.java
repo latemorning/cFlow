84fc17326087d699bae75add93becbbfa0fdfd81
kim hakjin <latemorning@gmail.com>
2024-07-30 13:52:30 +0900

    공통 응답객체

--- src/main/java/egovframework/com/cmm/util/ApiResponse.java
+++ src/main/java/egovframework/com/cmm/util/ApiResponse.java
@@ -9,24 +9,24 @@ import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
 public class ApiResponse<E> {
 
   private ApiHeader header;
-  private E[] datas;
+  private E datas;
   private PaginationInfo paginationInfo;
   private String msg;
 
   private static final int SUCCESS = 200;
 
-  private ApiResponse(ApiHeader header, E[] datas, PaginationInfo paginationInfo, String msg) {
+  private ApiResponse(ApiHeader header, E datas, PaginationInfo paginationInfo, String msg) {
     this.header = header;
     this.datas = datas;
     this.paginationInfo = paginationInfo;
     this.msg = msg;
   }
 
-  public static <E> ApiResponse<E> success(E[] datas, PaginationInfo paginationInfo, String message) {
+  public static <E> ApiResponse<E> success(E datas, PaginationInfo paginationInfo, String message) {
     return new ApiResponse<>(new ApiHeader(SUCCESS, "SUCCESS"), datas, paginationInfo, message);
   }
 
-  public static <E> ApiResponse<E> fail(ApiResponseCode responseCode, E[] datas) {
+  public static <E> ApiResponse<E> fail(ApiResponseCode responseCode, E datas) {
     return new ApiResponse<>(new ApiHeader(responseCode.getHttpStatusCode(), responseCode.getMessage()), datas, null,
         responseCode.getMessage());
   }
